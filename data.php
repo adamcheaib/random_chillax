09ab3ad7dc4272f492eb142b16f401cfee66f2d3
Adam <adam-cheaib@hotmail.com>
2023-04-16 00:47:09 +0200

    Randomized song added properly
    Final step is to have randomized GIF-backgrounds!

--- data.php
+++ data.php
@@ -1,5 +1,19 @@
 <?php
 
+function sendJSON($message, $http_code) {
+    header("content-type:application/json");
+    http_response_code($http_code);
+    echo json_encode($message);
+    exit();
+}
 
+$all_data = [];
+$scanned_folder = scandir("./sounds");
+$music_without_dots = array_splice($scanned_folder, 2);
+$music_data = shuffle($music_without_dots);
+$all_data["sounds"]["sources"] = $music_without_dots;
+$all_data["sounds"]["names"] = $music_without_dots;
 
+// var_dump($song_names);
+sendJSON($all_data, 200);
 ?>
--- script.js
+++ script.js
@@ -8,26 +8,48 @@ document.body.addEventListener("keydown", space_new_song);
 
 let time_counter = 0;
 
-function click_new_song(event) {
+let offline_data = [];
+fetch("./data.php").then(r => r.json()).then(data => offline_data.push(data));
+// async function fetch_images_and_songs() {
+//     try {
+//         const ALL_DATA = await (await fetch("./data.php")).json();
+//         offline_data.push(await ALL_DATA);
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
+
+
+// fetch_images_and_songs();
+console.log(offline_data);
+
+function click_new_song() {
+    const sounds_sources = offline_data[0].sounds.sources;
+    console.log(sounds_sources);
     time_counter = 0;
     background_display.style.opacity = "100%";
     main_button.style.opacity = "30%";
     main_button.addEventListener("mouseover", (event) => main_button.style.opacity = "100%");
-    main_button.addEventListener("mouseout", (event) => main_button.style.opacity = "30%");
-    bgm_music.src = "./sounds/Guustavv - Unsubmarine.mp3";
+    main_button.addEventListener("mouseout", (event) => main_button.style.opacity = "50%");
+
+    bgm_music.src = `./sounds/${sounds_sources[0]}`;
+    sounds_sources.splice(0, 1);
+    console.log(sounds_sources);
 
     setTimeout(() => {
         console.log(Math.floor(bgm_music.duration))
         bgm_music.play();
-        setInterval(() => {
+        const timeInterval = setInterval(() => {
+            console.log(time_counter);
             time_counter++;
-            if (Math.floor(bgm_music.duration) == time_counter) {
+            if (time_counter == Math.floor(bgm_music.duration)) {
                 console.log("DONE!");
                 time_counter = 0;
+                clearInterval(timeInterval);
+                click_new_song();
             }
-            console.log(time_counter);
         }, 1000);
-    }, 1000)
+    }, 1000);
 };
 
 function space_new_song(event) {
@@ -35,7 +57,7 @@ function space_new_song(event) {
         background_display.style.opacity = "100%";
         main_button.style.opacity = "30%";
         main_button.addEventListener("mouseover", (event) => main_button.style.opacity = "100%");
-        main_button.addEventListener("mouseout", (event) => main_button.style.opacity = "30%");
+        main_button.addEventListener("mouseout", (event) => main_button.style.opacity = "50%");
     }
 }
 
