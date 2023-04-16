19b3544279d859d45dbcd0c8fcbfe0b3c0b644c4
Adam <adam-cheaib@hotmail.com>
2023-04-16 12:06:45 +0200

    Randomized background images and song names
    Song name at the bottom etc.

--- data.php
+++ data.php
@@ -12,7 +12,10 @@ $scanned_music_folder = scandir("./sounds");
 $music_without_dots = array_splice($scanned_music_folder, 2);
 $music_data = shuffle($music_without_dots);
 $all_data["sounds"]["sources"] = $music_without_dots;
-$all_data["sounds"]["names"] = $music_without_dots;
+
+for ($i =0; $i < count($all_data["sounds"]["sources"]); $i++) {
+    $all_data["sounds"]["names"][$i] = str_replace("_", " ", pathinfo($all_data["sounds"]["sources"][$i], PATHINFO_FILENAME));
+}
 
 $scanned_image_folder = scandir("./media");
 $images_without_dots = array_splice($scanned_image_folder, 2);
--- script.js
+++ script.js
@@ -7,29 +7,34 @@ main_button.addEventListener("click", click_new_song);
 document.body.addEventListener("keydown", space_new_song);
 
 let time_counter = 0;
-
 let offline_data = [];
+
 fetch("./data.php").then(r => r.json()).then(data => offline_data.push(data));
 console.log(offline_data);
 
 function click_new_song() {
-    const sounds_sources = offline_data[0].sounds.sources;
-    const image_sources = offline_data[0].images;
     time_counter = 0;
+    const sounds_sources = offline_data[0].sounds.sources;
+    const sounds_names = offline_data[0].sounds.names;
+    const image_sources = offline_data[0].images;
     background_display.style.backgroundImage = `url(./media/${image_sources[0]})`; // Make a separate function for the images!
     background_display.style.opacity = "100%";
+
     main_button.style.opacity = "30%";
     main_button.addEventListener("mouseover", (event) => main_button.style.opacity = "100%");
-    main_button.addEventListener("mouseout", (event) => main_button.style.opacity = "50%");
+    main_button.addEventListener("mouseout", (event) => main_button.style.opacity = "30%");
 
     bgm_music.src = `./sounds/${sounds_sources[0]}`;
+    document.getElementById("artist").textContent = sounds_names[0];
     sounds_sources.splice(0, 1);
+    sounds_names.splice(0, 1);
     image_sources.splice(0, 1);
 
     setTimeout(() => {
         bgm_music.play();
         const timeInterval = setInterval(() => {
             time_counter++;
+            console.log(time_counter);
             if (time_counter == Math.floor(bgm_music.duration)) {
                 time_counter = 0;
                 clearInterval(timeInterval);
@@ -38,39 +43,44 @@ function click_new_song() {
         }, 1000);
     }, 1000);
 
-    if (document.body.key == "p") {
-        console.log("oh boi!");
-    }
+
 };
 
 function space_new_song(event) {
-    if (event.key == " ") {
-        const sounds_sources = offline_data[0].sounds.sources;
-        time_counter = 0;
-        background_display.style.opacity = "100%";
-        main_button.style.opacity = "30%";
-        main_button.addEventListener("mouseover", (event) => main_button.style.opacity = "100%");
-        main_button.addEventListener("mouseout", (event) => main_button.style.opacity = "50%");
+    time_counter = 0;
+    switch (event.key) {
+        case " ":
+            const sounds_sources = offline_data[0].sounds.sources;
+            const sounds_names = offline_data[0].sounds.names;
+            const image_sources = offline_data[0].images;
+            background_display.style.backgroundImage = `url(./media/${image_sources[0]})`;
+            background_display.style.opacity = "100%";
+            main_button.style.opacity = "30%";
+            main_button.addEventListener("mouseover", (event) => main_button.style.opacity = "100%");
+            main_button.addEventListener("mouseout", (event) => main_button.style.opacity = "50%");
 
-        bgm_music.src = `./sounds/${sounds_sources[0]}`;
-        sounds_sources.splice(0, 1);
+            bgm_music.src = `./sounds/${sounds_sources[0]}`;
+            document.getElementById("artist").textContent = sounds_names[0];
+            sounds_sources.splice(0, 1);
+            sounds_names.splice(0, 1);
+            image_sources.splice(0, 1);
 
-        setTimeout(() => {
-            bgm_music.play();
-            const timeInterval = setInterval(() => {
-                time_counter++;
-                if (time_counter == Math.floor(bgm_music.duration)) {
-                    time_counter = 0;
-                    clearInterval(timeInterval);
-                    click_new_song();
-                }
-            }, 1000);
-        }, 1000);
+            setTimeout(() => {
+                bgm_music.play();
+                const timeInterval = setInterval(() => {
+                    time_counter++;
+                    console.log(time_counter);
+                    if (time_counter == Math.floor(bgm_music.duration)) {
+                        time_counter = 0;
+                        clearInterval(timeInterval);
+                        click_new_song();
+                    }
+                }, 1000);
+            }, 1000); break;
 
-    };
-
-    // if (event.key == "ArrowUp") {
-    //     soun
-    // }
+        case "ArrowUp": bgm_music.volume += 0.1; break;
+        case "ArrowDown": bgm_music.volume -= 0.1; break;
+        case "p": bgm_music.paused ? bgm_music.play() : bgm_music.pause(); break;
+    }
 }
 
