28ed399cfaa0969388bade6b9bd655e0663aa6de
Adam <adam-cheaib@hotmail.com>
2023-04-19 00:30:14 +0200

    Set_Timeout removed
    Set_timeout was causing issues since it took a second for the clear interval to run etc. Now it is fixed!

--- script.js
+++ script.js
@@ -8,17 +8,14 @@ document.body.addEventListener("keydown", space_new_song);
 
 let time_counter = 0;
 let offline_data = [];
-let intervals = [];
+let currentInterval;
 
 fetch("./data.php").then(r => r.json()).then(data => offline_data.push(data));
 console.log(offline_data);
 
 function click_new_song() {
-    if (intervals.length != 0) {
-        clearInterval(intervals[0]);
-    };
-
     time_counter = 0;
+    clearInterval(currentInterval);
     const sounds_sources = offline_data[0].sounds.sources;
     const sounds_names = offline_data[0].sounds.names;
     const image_sources = offline_data[0].images;
@@ -35,56 +32,25 @@ function click_new_song() {
     sounds_names.splice(0, 1);
     image_sources.splice(0, 1);
 
-    setTimeout(() => {
-        bgm_music.play();
-        const timeInterval = setInterval(() => {
-            time_counter++;
-            console.log(time_counter);
-            if (time_counter == Math.floor(bgm_music.duration)) {
-                time_counter = 0;
-                clearInterval(timeInterval);
-                click_new_song();
-            }
-        }, 1000);
-        intervals.push(timeInterval);
+    bgm_music.play();
+    const timeInterval = setInterval(() => {
+        console.log(Math.floor(bgm_music.duration));
+        time_counter++;
+        console.log(time_counter);
+        if (time_counter == Math.floor(bgm_music.duration)) {
+            time_counter = 0;
+            clearInterval(timeInterval);
+            click_new_song();
+        }
     }, 1000);
+    currentInterval = timeInterval;
 };
 
 function space_new_song(event) {
-    if (intervals.length != 0) {
-        clearInterval(intervals[0]);
-    };
-    time_counter = 0;
     switch (event.key) {
         case " ":
-            const sounds_sources = offline_data[0].sounds.sources;
-            const sounds_names = offline_data[0].sounds.names;
-            const image_sources = offline_data[0].images;
-            background_display.style.backgroundImage = `url(./media/${image_sources[0]})`;
-            background_display.style.opacity = "100%";
-            main_button.style.opacity = "30%";
-            main_button.addEventListener("mouseover", (event) => main_button.style.opacity = "100%");
-            main_button.addEventListener("mouseout", (event) => main_button.style.opacity = "50%");
-
-            bgm_music.src = `./sounds/${sounds_sources[0]}`;
-            document.getElementById("artist").textContent = sounds_names[0];
-            sounds_sources.splice(0, 1);
-            sounds_names.splice(0, 1);
-            image_sources.splice(0, 1);
-
-            setTimeout(() => {
-                bgm_music.play();
-                const timeInterval = setInterval(() => {
-                    time_counter++;
-                    console.log(time_counter);
-                    if (time_counter == Math.floor(bgm_music.duration)) {
-                        time_counter = 0;
-                        clearInterval(timeInterval);
-                        click_new_song();
-                    }
-                }, 1000);
-                intervals.push(timeInterval);
-            }, 1000); break;
+            click_new_song();
+            break;
 
         case "ArrowUp": bgm_music.volume += 0.1; break;
         case "ArrowDown": bgm_music.volume -= 0.1; break;
